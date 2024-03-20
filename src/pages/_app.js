4ff4905fc44934c1f06ccd58048c00fa2d35d8c4
Akssses <itshnik07@gmail.com>
2024-03-21 04:29:08 +0600

    admin layout components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.scss";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <NextUIProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </NextUIProvider>
   );
 }
---
+++ src/pages/admin/notifications.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AdminNotificationPage() {
+  return <div>notification</div>;
+}
