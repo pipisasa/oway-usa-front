8568da035f78ddb1e463b406a7aef2acbbe9e889
aibek82 <aibekabykeev82@gmail.com>
2024-03-21 01:16:35 +0600

    pulled

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Register from "./auth/register";
 
 const inter = Inter({ subsets: ["latin"] });
 
