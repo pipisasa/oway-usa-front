3c3b0a068a84bd65716aafdc37616239e9a66278
Akssses <itshnik07@gmail.com>
2024-03-23 10:50:48 +0600

    pulled

--- src/components/screens/main/MoreServices.jsx
+++ src/components/screens/main/MoreServices.jsx
@@ -5,7 +5,7 @@ export default function MoreServices() {
       <div className={s.service}>
         <div className={`${s.service_container} container`}>
           <span>Дополнительные услуги</span>
-          <h2>Наши дополнительные сервисы</h2>
+          <h2>Наши дополнительные <br/> сервисы</h2>
           <div className={s.services_inner}>
             <div  className={s.services_inner_top}>
               <div>
@@ -16,7 +16,7 @@ export default function MoreServices() {
               </div>
               <div className={s.services_inner_top_block}>
                 <div className={s.services_span}>
-                  <div></div>
+                  <div className={s.services_inner_top_block_div}></div>
                   <span>Стоимость услуги: 5$</span>
                 </div>
                 <div className={s.services_span}>
@@ -38,7 +38,7 @@ export default function MoreServices() {
                   <span>Эта услуга включает в себя тщательную проверку различных устройств и механизмов с целью выявления любых дефектов или неисправностей, гарантируя, что оборудование функционирует должным образом и готово к использованию.</span>
                 </div>
                 <div className={s.services_span}>
-                  <div></div>
+                  <div  className={s.services_inner_top_block_div}></div>
                   <span>Услуга производится с помощью запросов через тикеты</span>
                 </div>
               </div>
