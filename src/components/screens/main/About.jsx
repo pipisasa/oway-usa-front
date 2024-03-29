7484f0caf058a9967258d89f6308ec4c96b824ab
aibek82 <aibekabykeev82@gmail.com>
2024-03-29 11:15:10 +0600

    pulled

---
+++ src/components/screens/main/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import s from "@/styles/screens/main/Search.module.scss";
+import Button from "../../partials/Button";
+
+export default function About() {
+    return (
+        <div className={s.heroSection_page}>
+            <div className={`${s.heroSection_page} container`}>
+                <div className={s.heroSection_info}>
+                    <h1>О компании</h1>
+                    <div className={s.heroSection_info_span}>
+                        <div></div>
+                        <span><strong>OWAY USA </strong> - логистическая компания осуществеляющая отправки коммерсечких и физических посылок из США и Турции в страны СНГ</span>
+                    </div>
+
+                </div>
+                <Button button="Закупиться"/>
+                <div className={s.heroSection_img1}>
+                    <img src="assets/images/teleshkaOrange.png" width={537} height={537} alt=""/>
+                </div>
+            </div>
+        </div>
+    );
+}
