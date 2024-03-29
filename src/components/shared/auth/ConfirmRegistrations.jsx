8d83ac68b122adc651f3618153fbf563a2646aa1
Akssses <itshnik07@gmail.com>
2024-03-29 10:12:09 +0600

    confitm calculator

--- src/components/shared/auth/ConfirmRegistrations.jsx
+++ src/components/shared/auth/ConfirmRegistrations.jsx
@@ -1,20 +1,57 @@
-import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
+import s from "@/styles/pages/auth/Register.module.scss";
 
-export default function ConfirmRegistrations() {
+export default function ConfirmRegistrations({ onSubmit }) {
+  const inputRefs = Array.from({ length: 6 }, () => useRef());
+
+  const handleInputChange = (index, e) => {
+    const input = e.target;
+    let value = input.value;
+
+    if (value.length > 1) {
+      value = value.slice(0, 1);
+      input.value = value;
+    }
+
+    if (value.length > 0) {
+      if (index < inputRefs.length - 1) {
+        inputRefs[index + 1].current.focus();
+      }
+    }
+  };
+
+  const handleKeyDown = (index, e) => {
+    if (e.key === "Backspace") {
+      const input = inputRefs[index].current;
+      const value = input.value;
+
+      if (!value && index > 0) {
+        inputRefs[index - 1].current.focus();
+      }
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit();
+  };
+
   return (
     <>
       <img className={s.steps} src="/assets/images/step3.svg" alt="step1" />
-      <form className={s.register_form}>
-        <div className={s.register_inputs}>
-          <div>
-            <label htmlFor="">Лицевая сторона паспорта</label>
-            <input type="file" />
-          </div>
-          <div>
-            <label htmlFor="">Обратная сторона паспорта</label>
-            <input type="file" />
-          </div>
+      <form className={s.register_form} onSubmit={handleSubmit}>
+        <p>Вам на почту пришел 6-ти значный код. Введите его:</p>
+        <div className={s.activation_inputs}>
+          {inputRefs.map((inputRef, index) => (
+            <input
+              key={index}
+              ref={inputRef}
+              type="text"
+              maxLength="1"
+              onChange={(e) => handleInputChange(index, e)}
+              onKeyDown={(e) => handleKeyDown(index, e)}
+            />
+          ))}
         </div>
         <div className={s.register_btn}>
           <button type="submit">
