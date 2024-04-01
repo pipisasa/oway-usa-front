4e3386d6619e91606ec0478171088eb9eb6fe25a
aibek82 <aibekabykeev82@gmail.com>
2024-04-01 12:09:46 +0600

    fix a bag

--- src/pages/faq/index.jsx
+++ src/pages/faq/index.jsx
@@ -7,7 +7,7 @@ import Feedback from "../../components/shared/Feedback";
 export default function FaqPage() {
     return (
         <div>
-            <div style={{width:"100%", height:"810px", borderRadius:"0px 0px 64px 64px",background: "var(--bue_light_2, #F7F9FC)"}}>
+            <div style={{width:"100%", height:"720px", borderRadius:"0px 0px 64px 64px",background: "var(--bue_light_2, #F7F9FC)"}}>
                 <Faq margin='no'/>
             </div>
             <Feedback/>
