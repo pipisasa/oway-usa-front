b82dca61560285bf38d44d1da8ee217850ee53c1
Akssses <itshnik07@gmail.com>
2024-04-05 07:55:45 +0600

    adaprive markets page;

--- src/pages/markets/index.jsx
+++ src/pages/markets/index.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-
 import Contacts from "../../components/partials/Contacts";
-
-import Faq from "../../components/partials/Faq";
 import Shops from "../../components/screens/main/Shops";
 import Markets from "../../components/screens/main/Markets";
 import StoresDinamyc from "../../components/partials/StoresDinamyc";
 import ProductSlider from "../../components/screens/main/ProductSlider";
 
 export default function MarketsPage() {
-    return (
-        <div>
-            <Markets/>
-            <ProductSlider/>
-            <StoresDinamyc span='Похожие магазины' h1='Мы выбрали для Вас лучшие онлайн-магазины'/>
-            <Shops/>
-            <Contacts/>
-        </div>
-    );
+  return (
+    <div>
+      <Markets />
+      <ProductSlider />
+      <StoresDinamyc
+        span="Похожие магазины"
+        h1="Мы выбрали для Вас лучшие онлайн-магазины"
+      />
+      <Shops />
+      <Contacts />
+    </div>
+  );
 }
