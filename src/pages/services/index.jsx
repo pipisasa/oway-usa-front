7edcb2715264f9e913c9bf5ce758c15713d3dbe8
Akssses <itshnik07@gmail.com>
2024-04-05 09:22:53 +0600

    adaptive services page perfomed

--- src/pages/services/index.jsx
+++ src/pages/services/index.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-
 import Contacts from "../../components/partials/Contacts";
-import Faq from "../../components/partials/Faq";
-import Feedback from "../../components/shared/Feedback";
 import Services from "../../components/screens/main/Services";
 import ProvideServices from "../../components/screens/main/ProvideServices";
 import Advantage from "../../components/screens/main/ Advantage";
 
 export default function ServicesPage() {
-    return (
-        <div>
-            <Services/>
-            <ProvideServices/>
-            <Advantage menu='service' h2='Оптимизируйте бюджет с нашими бесплатными услугами' p='Бесплатные услуги '/>
-            <Contacts/>
-        </div>
-    );
+  return (
+    <div>
+      <Services />
+      <ProvideServices />
+      <Advantage
+        menu="service"
+        h2="Оптимизируйте бюджет с нашими бесплатными услугами"
+        p="Бесплатные услуги "
+      />
+      <Contacts />
+    </div>
+  );
 }
