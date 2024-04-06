fe6b49ad4baf6f116f334d94599affd391d73217
Akssses <itshnik07@gmail.com>
2024-04-06 18:27:44 +0600

    main page adaptive perfomed

--- src/components/screens/main/AboutUs.jsx
+++ src/components/screens/main/AboutUs.jsx
@@ -1,33 +1,49 @@
 import React from "react";
 import s from "@/styles/screens/main/AboutUs.module.scss";
+
 export default function AboutUs() {
   return (
-      <div className={`${s.about} container`}>
-        <div className={s.about_info}>
-          <span>О компании</span>
-          <h1>OWAY USA</h1>
-          <div className={s.about_info_spans}>
-            <div className={s.about_info_span}>
-              <div className={s.about_info_div}></div>
-              <span> <strong>OWAY USA </strong> - сервис по доставке посылок и коммерческих грузов из США и Турции</span>
-            </div>
-            <div className={s.about_info_span}>
-              <div className={s.about_info_div1}></div>
-              <span>Мы имеем выгодные соглашения с авиалиниями, что дает нам возможность предоставлять нашим клиентам низкие цены за международную доставку.</span>
-            </div>
-            <div className={s.about_info_span}>
-              <div className={s.about_info_div2}></div>
-              <span>Сегодня <strong>OWAY USA </strong> имеет огромный опыт в доставке посылок и грузов, предоставляя услуги в Кыргызстан, Россия, Казахстан и Узбекистан.</span>
-            </div>
-            <div className={s.about_info_span}>
-              <div className={s.about_info_div3}></div>
-              <span>Мы предлагаем лучшие сроки и тарифы, которые отвечают Вашим потребностям, делая нас надежным партнером.</span>
-            </div>
+    <div className={`${s.about} container`}>
+      <div className={s.about_info}>
+        <span>О компании</span>
+        <h1>OWAY USA</h1>
+        <div className={s.about_info_spans}>
+          <div className={s.about_info_span}>
+            <div className={s.about_info_div}></div>
+            <span>
+              {" "}
+              <strong>OWAY USA </strong> - сервис по доставке посылок и
+              коммерческих грузов из США и Турции
+            </span>
           </div>
+          <div className={s.about_info_span}>
+            <div className={s.about_info_div1}></div>
+            <span>
+              Мы имеем выгодные соглашения с авиалиниями, что дает нам
+              возможность предоставлять нашим клиентам низкие цены за
+              международную доставку.
+            </span>
           </div>
-        <div className={s.about_img}>
-          <img src="assets/images/moto.png" alt=""/>
+          <div className={s.about_info_span}>
+            <div className={s.about_info_div2}></div>
+            <span>
+              Сегодня <strong>OWAY USA </strong> имеет огромный опыт в доставке
+              посылок и грузов, предоставляя услуги в Кыргызстан, Россия,
+              Казахстан и Узбекистан.
+            </span>
+          </div>
+          <div className={s.about_info_span}>
+            <div className={s.about_info_div3}></div>
+            <span>
+              Мы предлагаем лучшие сроки и тарифы, которые отвечают Вашим
+              потребностям, делая нас надежным партнером.
+            </span>
+          </div>
         </div>
       </div>
+      <div className={s.about_img}>
+        <img src="assets/images/moto.png" alt="" />
+      </div>
+    </div>
   );
 }
--- src/components/shared/TrackNumber.jsx
+++ src/components/shared/TrackNumber.jsx
@@ -4,21 +4,21 @@ import Button from "../partials/Button";
 
 export default function TrackNumber() {
   return (
-      <div className={`${s.track} container`}>
-        <div className={s.track_info}>
-            <div>
-                <span>Отслеживание доставки</span>
-                <h1>Отслеживайте свою доставку по введенному трек-номеру</h1>
-            </div>
-          <div className={s.track_info_input}>
-            <span>Трек-номер</span>
-            <input type="text" placeholder="Введите трек номер"/>
-          </div>
-            <Button button='Проверить'/>
+    <div className={`${s.track} container`}>
+      <div className={s.track_info}>
+        <div>
+          <span>Отслеживание доставки</span>
+          <h1>Отслеживайте свою доставку по введенному трек-номеру</h1>
         </div>
-        <div className={s.track_img}>
-            <img src="assets/images/dron.png" alt=""/>
+        <div className={s.track_info_input}>
+          <span>Трек-номер</span>
+          <input type="text" placeholder="Введите трек номер" />
         </div>
+        <button className={s.button}>Проверить</button>
       </div>
-  )
+      <div className={s.track_img}>
+        <img src="assets/images/dron.png" alt="" />
+      </div>
+    </div>
+  );
 }
