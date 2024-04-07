6fc998b4d4a29030105c55b9eaf315b05f2b2f47
Akssses <itshnik07@gmail.com>
2024-04-07 18:01:32 +0600

    user page adaptivka v2

--- src/components/shared/users/modals/BankCardsModal.jsx
+++ src/components/shared/users/modals/BankCardsModal.jsx
@@ -1,19 +1,16 @@
-// hooks/useRegister.js
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import s from "@/styles/admin/Modal.module.scss";
 import Modal from "@/components/shared/Modal";
-import { useBillingAdd } from "../../../../hooks/billing/useBillingAdd";
 import useBillingData from "../../../../hooks/billing/useBillingData";
 
 export default function BankCardsModal() {
   const [isOpen, setIsOpen] = useState(false);
   const [number, setNumber] = useState("");
-  const [end_date, setEnd_date] = useState("");
   const [full_name, setFull_name] = useState("");
   const [cardType, setCardType] = useState("");
   const [cvv, setCvv] = useState("");
 
-  const yearInputRef = useRef()
+  const yearInputRef = useRef();
   const { billingAdd } = useBillingData();
 
   const toggleModal = () => setIsOpen(!isOpen);
@@ -44,13 +41,12 @@ export default function BankCardsModal() {
   const [end_year, setEnd_year] = useState("");
 
   const handleMonthChange = (e) => {
-    let month = e.target.value.replace(/\D/g, ""); // Убираем все символы, кроме цифр
+    let month = e.target.value.replace(/\D/g, "");
     if (month.length > 2) {
-      month = month.slice(0, 2); // Ограничиваем длину до двух символов
+      month = month.slice(0, 2);
     }
 
     if (month.length === 2) {
-      // Если введено две цифры, переходим к году
       yearInputRef.current.focus();
     }
 
@@ -58,83 +54,91 @@ export default function BankCardsModal() {
   };
 
   const handleYearChange = (e) => {
-    let year = e.target.value.replace(/\D/g, ""); // Убираем все символы, кроме цифр
+    let year = e.target.value.replace(/\D/g, "");
     if (year.length > 4) {
-      year = year.slice(0, 4); // Ограничиваем длину до двух символов
+      year = year.slice(0, 4);
     }
 
     setEnd_year(year);
   };
 
-
   const submitBilling = () => {
     const formattedEnd_date = `${end_month}-${end_year}`;
-    billingAdd({ number, end_date: formattedEnd_date, full_name , cvv});
-    toggleModal()
-
+    billingAdd({ number, end_date: formattedEnd_date, full_name, cvv });
+    toggleModal();
   };
   return (
-      <div className={s.modal}>
-        <button onClick={toggleModal} className={s.add_btn}>
-          Добавить карту
-        </button>
-        <Modal isOpen={isOpen} onClose={toggleModal}>
-          <h3>Добавить карту</h3>
-          <form action="" className={s.cards_form} onSubmit={submitBilling}>
-            <div className={s.input_field}>
-              <label>Номер карты</label>
-              <input
-                  type="text"
-                  placeholder="Введите номер карты"
-                  value={number}
-                  onChange={handleCardNumberChange}
-              />
-              {cardType && <p>Тип карты: {cardType}</p>}
-            </div>
-            <div className={s.flex}>
-              <div>
-                <label>Срок действия</label>
-                <div className={s.cards_period}>
-                  <input
-                      className={s.input_month}
-                      value={end_month}
-                      type="text"
-                      placeholder="MM"
-                      maxLength="2"
-                      onChange={handleMonthChange}
-                  />
-                  <input
-                      ref={yearInputRef}
-                      className={s.input_year}
-                      type="text"
-                      placeholder="YY"
-                      maxLength="4"
-                      onChange={handleYearChange}
-                  />
-                </div>
-              </div>
-              <div className={s.input_field}>
-                <label>CVV код</label>
-                <input value={cvv} type="text" maxLength="3" onChange={(e) => setCvv(e.target.value)} placeholder="CVV" />
-              </div>
-            </div>
-
-            <div className={s.input_field}>
-              <label>Имя на карте</label>
-              <input
-                  value={full_name}
-                  type="text"
-                  placeholder="Введите имя"
-                  onChange={(e) => setFull_name(e.target.value)}
-              />
-            </div>
-          </form>
-          <div className={s.btn_center}>
-            <button onClick={submitBilling} type="submit" className={s.submit_btn}>
-              Добавить карту
-            </button>
+    <div className={s.modal}>
+      <button onClick={toggleModal} className={s.add_btn}>
+        Добавить карту
+      </button>
+      <Modal isOpen={isOpen} onClose={toggleModal}>
+        <h3>Добавить карту</h3>
+        <form action="" className={s.cards_form} onSubmit={submitBilling}>
+          <div className={s.input_field}>
+            <label>Номер карты</label>
+            <input
+              type="text"
+              placeholder="Введите номер карты"
+              value={number}
+              onChange={handleCardNumberChange}
+            />
+            {cardType && <p>Тип карты: {cardType}</p>}
           </div>
-        </Modal>
-      </div>
+          <div className={s.flex}>
+            <div>
+              <label>Срок действия</label>
+              <div className={s.cards_period}>
+                <input
+                  className={s.input_month}
+                  value={end_month}
+                  type="text"
+                  placeholder="MM"
+                  maxLength="2"
+                  onChange={handleMonthChange}
+                />
+                <input
+                  ref={yearInputRef}
+                  className={s.input_year}
+                  type="text"
+                  placeholder="YY"
+                  maxLength="4"
+                  onChange={handleYearChange}
+                />
+              </div>
+            </div>
+            <div className={s.input_field}>
+              <label>CVV код</label>
+              <input
+                value={cvv}
+                type="text"
+                maxLength="3"
+                onChange={(e) => setCvv(e.target.value)}
+                placeholder="CVV"
+              />
+            </div>
+          </div>
+
+          <div className={s.input_field}>
+            <label>Имя на карте</label>
+            <input
+              value={full_name}
+              type="text"
+              placeholder="Введите имя"
+              onChange={(e) => setFull_name(e.target.value)}
+            />
+          </div>
+        </form>
+        <div className={s.btn_center}>
+          <button
+            onClick={submitBilling}
+            type="submit"
+            className={s.submit_btn}
+          >
+            Добавить карту
+          </button>
+        </div>
+      </Modal>
+    </div>
   );
 }
