1d56da29fa8d5d95bbc7713cd764a4bf4c6ca606
Akssses <itshnik07@gmail.com>
2024-04-09 17:21:54 +0600

    fixed changes

--- src/hooks/admin/useShops.js
+++ src/hooks/admin/useShops.js
@@ -4,143 +4,138 @@ import axios from "axios";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const useShops = (selectedCategory) => {
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isSuccess, setIsSuccess] = useState(false);
-    const [products, setProducts] = useState([]);
-    const filtercategory = 2
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [products, setProducts] = useState([]);
+  const filtercategory = 2;
 
+  useEffect(() => {
+    fetchShops(selectedCategory);
+  }, [selectedCategory]);
 
-    useEffect(() => {
-        fetchShops(selectedCategory);
-    }, [selectedCategory]);
+  const fetchShops = async (selectedCategory) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await axios.get(
+        `${API_URL}/api/catalog/sites/list/${
+          selectedCategory ? `?category=${selectedCategory}` : ""
+        }`
+      );
 
-    const fetchShops = async (selectedCategory) => {
-        const accessToken = getCookie("accessToken");
-        setIsLoading(true);
-        setError(null);
-        try {
-            const response = await axios.get(
-                `${API_URL}/api/catalog/sites/list/${selectedCategory ? `?category=${selectedCategory}` : ""}`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                    }
-                }
-            );
+      setProducts(response.data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-            setProducts(response.data);
-        } catch (err) {
-            setError(err.message);
-        } finally {
-            setIsLoading(false);
+  useEffect(() => {
+    fetchShops();
+  }, []);
+
+  const addShops = async (name, category, country, logo, description, url) => {
+    const accessToken = getCookie("accessToken");
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("category", category);
+    formData.append("country", country);
+    formData.append("logo", logo);
+    formData.append("description", description);
+    formData.append("url", url);
+
+    setIsLoading(true);
+    setError(null);
+    setIsSuccess(false);
+
+    try {
+      const response = await axios.post(
+        `${API_URL}/api/catalog/sites/create/`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "multipart/form-data",
+          },
         }
-    };
+      );
 
-    useEffect(() => {
-        fetchShops();
-    }, []);
+      await fetchShops();
+      setIsSuccess(true);
+    } catch (error) {
+      setError(error.message);
+      setIsSuccess(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    const addShops = async (name, category, country, logo, description, url) => {
-        const accessToken = getCookie("accessToken");
-        const formData = new FormData();
-        formData.append("name", name);
-        formData.append("category", category);
-        formData.append("country", country);
-        formData.append("logo", logo);
-        formData.append("description", description);
-        formData.append("url", url);
+  const deleteShops = async (productId) => {
+    setIsLoading(true);
+    setError(null);
 
-        setIsLoading(true);
-        setError(null);
-        setIsSuccess(false);
-
-        try {
-            const response = await axios.post(
-                `${API_URL}/api/catalog/sites/create/`,
-                formData,
-                {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                        "Content-Type": "multipart/form-data",
-                    },
-                }
-            );
-
-            await fetchShops();
-            setIsSuccess(true);
-        } catch (error) {
-            setError(error.message);
-            setIsSuccess(false);
-        } finally {
-            setIsLoading(false);
+    try {
+      const accessToken = getCookie("accessToken");
+      const response = await axios.delete(
+        `${API_URL}/api/catalog/sites/delete/${productId}/`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
         }
-    };
+      );
 
-    const deleteShops = async (productId) => {
-        setIsLoading(true);
-        setError(null);
+      setProducts((currentProducts) =>
+        currentProducts.filter((product) => product.id !== productId)
+      );
+      fetchShops();
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-        try {
-            const accessToken = getCookie("accessToken");
-            const response = await axios.delete(
-                `${API_URL}/api/catalog/sites/delete/${productId}/`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                    },
-                }
-            );
+  const updateShops = async (productId, formData) => {
+    setIsLoading(true);
+    setError(null);
 
-            setProducts((currentProducts) =>
-                currentProducts.filter((product) => product.id !== productId)
-            );
-            fetchShops();
-        } catch (error) {
-            setError(error.message);
-        } finally {
-            setIsLoading(false);
+    try {
+      const accessToken = getCookie("accessToken");
+      const response = await axios.patch(
+        `${API_URL}/api/catalog/sites/update/${productId}/`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
         }
-    };
+      );
+      fetchShops();
+      const updatedProduct = response.data;
+      setProducts((currentProducts) =>
+        currentProducts.map((product) =>
+          product.id === productId ? { ...product, ...updatedProduct } : product
+        )
+      );
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    const updateShops = async (productId, formData) => {
-        setIsLoading(true);
-        setError(null);
-
-        try {
-            const accessToken = getCookie("accessToken");
-            const response = await axios.patch(
-                `${API_URL}/api/catalog/sites/update/${productId}/`,
-                formData,
-                {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                    },
-                }
-            );
-            fetchShops();
-            const updatedProduct = response.data;
-            setProducts((currentProducts) =>
-                currentProducts.map((product) =>
-                    product.id === productId ? { ...product, ...updatedProduct } : product
-                )
-            );
-        } catch (error) {
-            setError(error.message);
-        } finally {
-            setIsLoading(false);
-        }
-    };
-
-    return {
-        products,
-        addShops,
-        deleteShops,
-        fetchShops,
-        updateShops,
-        error,
-        isLoading,
-    };
+  return {
+    products,
+    addShops,
+    deleteShops,
+    fetchShops,
+    updateShops,
+    error,
+    isLoading,
+  };
 };
 
 export default useShops;
