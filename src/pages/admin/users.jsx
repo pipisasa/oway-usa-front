6eb2cafc3cf1bd01a67bf444c4a368384ad28296
Zhaparaliev Aktan <120387432+japaqwer@users.noreply.github.com>
2024-04-12 04:56:19 +0600

    pulled

--- src/pages/admin/users.jsx
+++ src/pages/admin/users.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import s from "@/styles/pages/admin/AdminUsersPage.module.scss";
+import c from "@/styles/pages/admin/AdminWareHousesPage.module.scss";
 import { Pagination } from "@nextui-org/react";
 import Modal from "../../components/shared/Modal";
 import { RxCross2 } from "react-icons/rx";
@@ -10,6 +11,8 @@ const PAGE_SIZE = 7;
 
 export default function AdminUsersPage() {
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchPhoneNumber, setSearchPhoneNumber] = useState("");
+  const [searchUniqueId, setSearchUniqueId] = useState("");
   const { users, isLoading, fetchUsers } = useUsersAdmin(currentPage);
   const [selectedUser, setSelectedUser] = useState(null);
   const [isLoadingPage, setIsLoadingPage] = useState(false);
@@ -24,81 +27,108 @@ export default function AdminUsersPage() {
   }
 
   return (
-    <div className={s.users_page}>
-      <table>
-        <thead>
-          <tr>
-            <th>Имя</th>
-            <th>Фамилия</th>
-            <th>Почта</th>
-            <th>Номер телефона</th>
-            <th>Уникальный ID</th>
-            <th>Данные ID паспорта</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users?.results
-            ?.filter((item) => item.id !== 1)
-            .map((user) => (
-              <tr key={user.id}>
-                <td>{user.first_name}</td>
-                <td>{user.last_name}</td>
-                <td>{user.email}</td>
-                <td>{user.phone_number}</td>
-                <td>#{user.unique_id}</td>
-                <td>
-                  <button
-                    className={s.btn}
-                    onClick={() => setSelectedUser(user)}
-                  >
-                    Подробнее
-                  </button>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-      {selectedUser && (
-        <div className={s.modal}>
-          <Modal isOpen={selectedUser} onClose={() => setSelectedUser(null)}>
-            <div className={s.modalContent}>
-              <div className={s.btn_center}>
-                <button
-                  onClick={() => setSelectedUser(null)}
-                  className={s.close_btn}
-                >
-                  <RxCross2 size={20} />
-                </button>
-              </div>
-              <h3>Данные ID паспорта</h3>
-              <div>
-                {selectedUser.front_image && selectedUser.back_image ? (
-                  <>
-                    <img
-                      src={`http://18.222.184.72:8000/${selectedUser.front_image}`}
-                      alt=""
-                    />
-                    <img
-                      src={`http://18.222.184.72:8000/${selectedUser.back_image}`}
-                      alt=""
-                    />
-                  </>
-                ) : (
-                  <p>Пользователь не загрузил паспортные данные</p>
-                )}
-              </div>
-            </div>
-          </Modal>
+    <>
+      <div style={{ display: "flex", gap: "20px" }} className={c.warehouses_page}>
+        <div className={c.search}>
+          <img src="/assets/icons/search.svg" alt="icon" />
+          <input
+            type="text"
+            placeholder="Поиск по номеру телефона"
+            value={searchPhoneNumber}
+            onChange={(e) => setSearchPhoneNumber(e.target.value)}
+          />
         </div>
-      )}
-      <div className={s.pagination}>
-        <Pagination
-          variant="bordered"
-          total={Math.ceil(users.count / PAGE_SIZE)}
-          initialPage={currentPage}
-          onChange={(page) => setCurrentPage(page)}
-        />
+        <div className={c.search}>
+          <img src="/assets/icons/search.svg" alt="icon" />
+          <input
+            type="text"
+            placeholder="Поиск по уникальному ID"
+            value={searchUniqueId}
+            onChange={(e) => setSearchUniqueId(e.target.value)}
+          />
+        </div>
       </div>
-    </div>
+
+      <div className={s.users_page}>
+        <table>
+          <thead>
+            <tr>
+              <th>Имя</th>
+              <th>Фамилия</th>
+              <th>Почта</th>
+              <th>Номер телефона</th>
+              <th>Уникальный ID</th>
+              <th>Данные ID паспорта</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.results
+              ?.filter((item) => item.id !== 1)
+              .filter((user) =>
+                user.phone_number.includes(searchPhoneNumber) &&
+                user.unique_id.includes(searchUniqueId)
+              )
+              .map((user) => (
+                <tr key={user.id}>
+                  <td>{user.first_name}</td>
+                  <td>{user.last_name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone_number}</td>
+                  <td>#{user.unique_id}</td>
+                  <td>
+                    <button
+                      className={s.btn}
+                      onClick={() => setSelectedUser(user)}
+                    >
+                      Подробнее
+                    </button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        {selectedUser && (
+          <div className={s.modal}>
+            <Modal isOpen={selectedUser} onClose={() => setSelectedUser(null)}>
+              <div className={s.modalContent}>
+                <div className={s.btn_center}>
+                  <button
+                    onClick={() => setSelectedUser(null)}
+                    className={s.close_btn}
+                  >
+                    <RxCross2 size={20} />
+                  </button>
+                </div>
+                <h3>Данные ID паспорта</h3>
+                <div>
+                  {selectedUser.front_image && selectedUser.back_image ? (
+                    <>
+                      <img
+                        src={`http://18.222.184.72:8000/${selectedUser.front_image}`}
+                        alt=""
+                      />
+                      <img
+                        src={`http://18.222.184.72:8000/${selectedUser.back_image}`}
+                        alt=""
+                      />
+                    </>
+                  ) : (
+                    <p>Пользователь не загрузил паспортные данные</p>
+                  )}
+                </div>
+              </div>
+            </Modal>
+          </div>
+        )}
+        <div className={s.pagination}>
+          <Pagination
+            variant="bordered"
+            total={Math.ceil(users.count / PAGE_SIZE)}
+            initialPage={currentPage}
+            onChange={(page) => setCurrentPage(page)}
+          />
+        </div>
+      </div>
+    </>
   );
 }
