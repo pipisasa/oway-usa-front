178f3cdbcce7f4d5fecc63b1a31e30ecff0272fd
Zhaparaliev Aktan <120387432+japaqwer@users.noreply.github.com>
2024-04-15 13:38:56 +0600

    pulled

--- src/pages/user/my-parcels.jsx
+++ src/pages/user/my-parcels.jsx
@@ -1,19 +1,29 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import s from "@/styles/pages/user/MyWarehouse.module.scss";
 import useWarehouses from "@/hooks/user/useWarehouses";
 import Loading from "@/components/shared/admin/Loading";
 import MyWarehousesEditModal from "@/components/shared/admin/modals/MyWarehousesEditModal";
+import {Pagination} from "@nextui-org/react";
 
 export default function MyWarehouses() {
-  const { products, isLoading, deleteWarehouses } = useWarehouses();
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedNotification, setSelectedNotification] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
   const [searchPhoneNumber, setSearchPhoneNumber] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [countryFilter, setCountryFilter] = useState("");
+  const { products, isLoading, deleteWarehouses, fetchWarehouses, total  } = useWarehouses(currentPage);
+
+  useEffect(() => {
+    fetchWarehouses(currentPage)
+  }, [currentPage]);
+
   if (isLoading) {
     return <Loading />;
   }
+  console.log(total,89)
+
+
   const handleUpdate = async (productId) => {
     try {
       setSelectedNotification(
@@ -35,67 +45,75 @@ export default function MyWarehouses() {
   };
 
   return (
-    <section className={s.my}>
-      <div className={s.filters}>
-        <div className={s.search}>
-          <img src="/assets/icons/search.svg" alt="icon"/>
-          <input
-              type="text"
-              placeholder="Поиск по трак номеру"
-              value={searchPhoneNumber}
-              onChange={(e) => setSearchPhoneNumber(e.target.value)}
+      <section className={s.my}>
+        <div className={s.filters}>
+          <div className={s.search}>
+            <img src="/assets/icons/search.svg" alt="icon"/>
+            <input
+                type="text"
+                placeholder="Поиск по трак номеру"
+                value={searchPhoneNumber}
+                onChange={(e) => setSearchPhoneNumber(e.target.value)}
+            />
+          </div>
+          <select
+              className={s.select}
+              value={countryFilter}
+              onChange={(e) => setCountryFilter(e.target.value)}
+          >
+            <option value="">Страна отправки</option>
+            <option value="США">США</option>
+            <option value="Турция">Турция</option>
+          </select>
+        </div>
+        <table>
+          <thead>
+          <tr>
+            <th>Склад</th>
+            <th>Трак номер</th>
+            <th>Курьерская служба</th>
+            <th>Действие</th>
+          </tr>
+          </thead>
+          <tbody>
+          {products?.filter(
+              (products) =>
+                  products.tracking_number.includes(searchPhoneNumber))
+              .filter((product) =>
+                  countryFilter ? product.warehouse === countryFilter : true
+              )
+              .map((product) => (
+                  <tr key={product.id}>
+                    <td>{product.warehouse}</td>
+                    <td>{product.tracking_number}</td>
+                    <td>{product.courier_service}</td>
+                    <td className={s.actions}>
+                      <button onClick={() => deleteWarehouses(product.id)}>
+                        <img src="/assets/icons/delete.svg" alt="delete"/>
+                      </button>
+                      <button onClick={() => handleUpdate(product.id)}>
+                        <img src="/assets/icons/edit.svg" alt="edit"/>
+                      </button>
+                    </td>
+                  </tr>
+              ))}
+          </tbody>
+        </table>
+        {isEditing && (
+            <MyWarehousesEditModal
+                isOpen={isModalOpen}
+                onClose={() => toggleModal(false)}
+                warehouse={selectedNotification}
+            />
+        )}
+        <div className={s.pagination}>
+          <Pagination
+              variant="bordered"
+              total={total}
+              initialPage={currentPage}
+              onChange={(page) => setCurrentPage(page)}
           />
         </div>
-        <select
-            className={s.select}
-            value={countryFilter}
-            onChange={(e) => setCountryFilter(e.target.value)}
-        >
-          <option value="">Страна отправки</option>
-          <option value="США">США</option>
-          <option value="Турция">Турция</option>
-        </select>
-      </div>
-      <table>
-        <thead>
-        <tr>
-          <th>Склад</th>
-          <th>Трак номер</th>
-          <th>Курьерская служба</th>
-          <th>Действие</th>
-        </tr>
-        </thead>
-        <tbody>
-        {products?.filter(
-            (products) =>
-                products.tracking_number.includes(searchPhoneNumber))
-            .filter((product) =>
-                countryFilter ? product.warehouse === countryFilter : true
-            )
-            .map((product) => (
-            <tr key={product.id}>
-              <td>{product.warehouse}</td>
-              <td>{product.tracking_number}</td>
-              <td>{product.courier_service}</td>
-              <td className={s.actions}>
-                <button onClick={() => deleteWarehouses(product.id)}>
-                  <img src="/assets/icons/delete.svg" alt="delete" />
-                </button>
-                <button onClick={() => handleUpdate(product.id)}>
-                  <img src="/assets/icons/edit.svg" alt="edit" />
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {isEditing && (
-          <MyWarehousesEditModal
-              isOpen={isModalOpen}
-              onClose={() => toggleModal(false)}
-              warehouse={selectedNotification}
-          />
-      )}
-    </section>
+      </section>
   );
 }
