885ea2194437cd5725290d18c2617201b2c98c21
Пользователь <polzovatel@MacBook-Pro-Polzovatel.local>
2024-06-14 23:28:36 +0600

    qweqwe

--- src/components/partials/about/News.jsx
+++ src/components/partials/about/News.jsx
@@ -2,13 +2,16 @@ import React, { useRef } from "react";
 import s from "@/styles/partials/News.module.scss";
 import NewsCards from "./NewsCards";
 import useBulletinBoard from "@/hooks/admin/useBulletinBoard";
+import useBulletinBoardCategory from "@/hooks/admin/useBulletinBoardCategory";
 
 export default function News() {
   const { bulletins, loading, error } = useBulletinBoard();
+  const { bulletins1 } = useBulletinBoardCategory();
   const containerRef = useRef(null);
 
   if (loading) return <p>Загрузка...</p>;
   if (error) return <p>Ошибка: {error}</p>;
+  console.log(bulletins);
 
   const bulletinsByCity = bulletins.reduce((acc, bulletin) => {
     if (!acc[bulletin.city]) {
@@ -50,7 +53,12 @@ export default function News() {
       </div>
       <div className={s.news_cards} ref={containerRef}>
         {Object.keys(bulletinsByCity).map((city) => (
-          <NewsCards key={city} city={city} bulletins={bulletinsByCity[city]} />
+          <NewsCards
+            key={city}
+            city={city}
+            bulletins1={bulletins1}
+            bulletins={bulletinsByCity[city]}
+          />
         ))}
       </div>
     </section>
--- src/components/partials/about/NewsCards.jsx
+++ src/components/partials/about/NewsCards.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import s from "@/styles/partials/NewsCards.module.scss";
 
-export default function NewsCards({ city, bulletins }) {
+export default function NewsCards({ city, bulletins, bulletins1 }) {
   return (
     <section className={s.cards_block}>
       <h3>{city}</h3>
-      {bulletins.map((bulletin) => (
-        <div key={bulletin.id} className={s.card}>
-          <div className={s.header}>
-            <h5 style={{ backgroundColor: `${bulletin.color}` }}>
-              {bulletin.category}
-            </h5>
-            <span>{bulletin.publication_date}</span>
+      {bulletins.map((bulletin) => {
+        const matchingBulletin = bulletins1.find(
+          (item) => item.id === bulletin.item_category
+        );
+
+        return matchingBulletin ? (
+          <div key={bulletin.id} className={s.card}>
+            <div className={s.header}>
+              <h5 style={{ backgroundColor: `${matchingBulletin.color}` }}>
+                {matchingBulletin.name}
+              </h5>
+              <span>{bulletin.publication_date}</span>
+            </div>
+            <p>{bulletin.text}</p>
           </div>
-          <p>{bulletin.text}</p>
-        </div>
-      ))}
+        ) : null;
+      })}
     </section>
   );
 }
