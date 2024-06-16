eb5cdc63ce3777d7f850c0c044c03f5c23b1b58b
Пользователь <polzovatel@MacBook-Pro-Polzovatel.local>
2024-06-16 20:08:05 +0600

    eerwerw

--- src/components/shared/admin/modals/ImageModal.jsx
+++ src/components/shared/admin/modals/ImageModal.jsx
@@ -4,16 +4,18 @@ import { RxCross1 } from "react-icons/rx";
 
 export default function ImageModal({ src, onClose }) {
   return (
-    <div
-      className={s.modal_backdrop}
-      onClick={(e) => e.target === e.currentTarget && onClose()}
-    >
-      <div className={s.image_modal}>
-        <button onClick={onClose} className={s.exit}>
-          <RxCross1 size={22} />
-        </button>
-        <img src={src} alt="Zoomed" />
+    <>
+      <div
+        className={s.modal_backdrop}
+        onClick={(e) => e.target === e.currentTarget && onClose()}
+      >
+        <div className={s.image_modal}>
+          <button onClick={onClose} className={s.exit}>
+            <RxCross1 size={22} />
+          </button>
+          <img src={src} alt="Zoomed" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
